b812760fcb94a20ca5edc9ad306bfa54f8cc1071
Mike Readhead <masked.jokertdc@gmail.com>
2013-12-23 13:10:34 +0000

    Pacman

--- Python_Examples/Blinky.py
+++ Python_Examples/Blinky.py
@@ -4,7 +4,8 @@ class PiLiteBoard(object):
     def __init__(self):
         self.ser = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
         self.ser.write("$$$SPEED50\r")
-
+        self.ser.write("$$$F000000000000000000000111000011111110011111110111111111111101111111101111011000110011000110000000000000000000000000000000000000")
+        sleep(3)
     def write(self, text):
         text = text.encode('utf-8')
         while text:
