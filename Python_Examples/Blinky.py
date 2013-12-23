722daeacb88e9dafdc1543a9878e4d7bc0d858ca
Mike Readhead <masked.jokertdc@gmail.com>
2013-12-23 13:08:20 +0000

    New Blinky.

--- Python_Examples/Blinky.py
+++ Python_Examples/Blinky.py
@@ -1,4 +1,17 @@
-from PiLiteLib import PiLiteBoard
+import serial
+
+class PiLiteBoard(object):
+    def __init__(self):
+        self.ser = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
+        self.ser.write("$$$SPEED50\r")
+
+    def write(self, text):
+        text = text.encode('utf-8')
+        while text:
+            self.ser.write(text[:14])
+            text = text[14:]
+            sleep(3)
+
 
 def main():
 	pi = PiLiteBoard()
