7acb3e004ee93e4ab03251d68518ca6f66286368
Mike Readhead <masked.jokertdc@gmail.com>
2013-12-23 13:15:31 +0000

    Whitespace

--- Python_Examples/Blinky.py
+++ Python_Examples/Blinky.py
@@ -5,19 +5,19 @@ class PiLiteBoard(object):
     def __init__(self):
         self.ser = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
         self.ser.write("$$$SPEED50\r")
-        self.ser.write("$$$F000000000000000000000111000011111110011111110111111111111101111111101111011000110011000110000000000000000000000000000000000000")
-        sleep(3)
     def write(self, text):
         text = text.encode('utf-8')
         while text:
             self.ser.write(text[:14])
             text = text[14:]
             sleep(3)
+    def pacman(self):
+        self.ser.write("$$$F000000000000000000000111000011111110011111110111111111111101111111101111011000110011000110000000000000000000000000000000000000")
 
 
 def main():
-	pi = PiLiteBoard()
-	pi.write("Hello")
+    pi = PiLiteBoard()
+    pi.pacman()
 
 if __name__ == "__main__":
-	main()
+    main()
