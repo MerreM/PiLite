ae9d828bbe3dc5d1ffddb1eb918f04930d8508b0
Mike Readhead <masked.jokertdc@gmail.com>
2013-12-23 13:17:29 +0000

    Pacman

--- Python_Examples/Blinky.py
+++ Python_Examples/Blinky.py
@@ -5,6 +5,7 @@ class PiLiteBoard(object):
     def __init__(self):
         self.ser = serial.Serial("/dev/ttyAMA0", baudrate=9600, timeout=3.0)
         self.ser.write("$$$SPEED50\r")
+        self.ser.write("$$$ALL,OFF\r")
     def write(self, text):
         text = text.encode('utf-8')
         while text:
@@ -12,7 +13,7 @@ class PiLiteBoard(object):
             text = text[14:]
             sleep(3)
     def pacman(self):
-        self.ser.write("$$$F000000000000000000000111000011111110011111110111111111111101111111101111011000110011000110000000000000000000000000000000000000")
+        self.ser.write("$$$F000000000000000000000111000011111110011111110111111111111101111111101111011000110011000110000000000000000000000000000000000000\r")
 
 
 def main():
